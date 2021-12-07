56ab026ca134898e0f5978fc43fa7dde631032b3
Michał Myller <michalmyll@gmail.com>
2021-12-07 17:51:55 +0100

    day07 solution

---
+++ day07/main.py
@@ -0,0 +1,26 @@
+import fire
+import numpy as np
+
+
+def part_one(data: np.ndarray):
+    # min_value, max_value = np.min(data), np.max(data)
+    median = np.median(data)
+    fuel = np.sum(np.abs(data - median))
+    print(f"Answer 1: {fuel}")
+
+
+def part_two(data: np.ndarray):
+    mean = int(np.mean(data))
+    position_changes = np.abs(data - mean)
+    fuel = np.sum((position_changes / 2) * (1 + position_changes))
+    print(f"Answer 2: {fuel}")
+
+
+def main(input_file_path: str):
+    data = np.loadtxt(input_file_path, delimiter=',', dtype=int)
+    part_one(data)
+    part_two(data)
+
+
+if __name__ == '__main__':
+    fire.Fire(main)
