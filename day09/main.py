43ece7c6758a09b705f86f3b50ca842d72360f68
Michal Myller <michalmyll@gmail.com>
2021-12-09 15:25:06 +0100

    added solution for day09

--- day09/main.py
+++ day09/main.py
@@ -8,13 +8,6 @@ X = 0
 Y = 1
 
 
-def main(input_file_path: str):
-    data = pd.read_csv(input_file_path, dtype=str, header=None)
-    data = data[0].apply(lambda x: pd.Series(list(x))).astype(int).values
-    part_one(data)
-    part_two(data)
-
-
 def find_low_points(data):
     ii, jj = np.arange(len(data)), np.arange(data.shape[1])
     data = np.pad(data, (1, 1), constant_values=10)
@@ -35,28 +28,55 @@ def part_one(data):
     print(f"Answer 1: {np.sum(low_points + 1)}")
 
 
-def find_basin_points(starting_point, data):
-    valid_neighbours = set()
+def find_basin_points(starting_point, data, result):
+    valid_neighbours = find_valid_neighbours(data, starting_point)
+    for neighbour in valid_neighbours:
+        result.add(neighbour)
+        find_basin_points(neighbour, data, result)
+    return result
+
+
+def find_valid_neighbours(data, starting_point):
+    valid_neighbours = []
     if starting_point[X] - 1 >= 0:
-        if data[starting_point[X] - 1, starting_point[Y]] > data[starting_point[X], starting_point[Y]]:
-            valid_neighbours.add((starting_point[X] - 1, starting_point[Y]))
+        if data[starting_point[X] - 1, starting_point[Y]] > data[
+            starting_point[X], starting_point[Y]] and data[
+            starting_point[X] - 1, starting_point[Y]] != 9:
+            valid_neighbours.append((starting_point[X] - 1, starting_point[Y]))
     if starting_point[X] + 1 < BOUNDS[X]:
-        if data[starting_point[X] + 1, starting_point[Y]] > data[starting_point[X], starting_point[Y]]:
-            valid_neighbours.add((starting_point[X] + 1, starting_point[Y]))
+        if data[starting_point[X] + 1, starting_point[Y]] > data[
+            starting_point[X], starting_point[Y]] and data[
+            starting_point[X] + 1, starting_point[Y]] != 9:
+            valid_neighbours.append((starting_point[X] + 1, starting_point[Y]))
     if starting_point[Y] - 1 >= 0:
-        if data[starting_point[X], starting_point[Y] - 1] > data[starting_point[X], starting_point[Y]]:
-            valid_neighbours.add((starting_point[X], starting_point[Y] - 1))
-    if starting_point[X] + 1 < BOUNDS[X]:
-        if data[starting_point[X], starting_point[Y] + 1] > data[starting_point[X], starting_point[Y]]:
-            valid_neighbours.add((starting_point[X], starting_point[Y] + 1))
-    a=5
+        if data[starting_point[X], starting_point[Y] - 1] > data[
+            starting_point[X], starting_point[Y]] and data[
+            starting_point[X], starting_point[Y] - 1] != 9:
+            valid_neighbours.append((starting_point[X], starting_point[Y] - 1))
+    if starting_point[Y] + 1 < BOUNDS[Y]:
+        if data[starting_point[X], starting_point[Y] + 1] > data[
+            starting_point[X], starting_point[Y]] and data[
+            starting_point[X], starting_point[Y] + 1] != 9:
+            valid_neighbours.append((starting_point[X], starting_point[Y] + 1))
+    return valid_neighbours
 
 
 def part_two(data):
     low_points_positions = find_low_points(data)
+    basin_points_lengths = []
     for low_point in low_points_positions:
-        find_basin_points(low_point, data)
-        a=5
+        result = set()
+        basin_points = find_basin_points(low_point, data, result)
+        basin_points_lengths.append(len(basin_points) + 1)
+    top_3 = sorted(basin_points_lengths, reverse=True)[:3]
+    print(f"Answer 2: {np.prod(top_3)}")
+
+
+def main(input_file_path: str):
+    data = pd.read_csv(input_file_path, dtype=str, header=None)
+    data = data[0].apply(lambda x: pd.Series(list(x))).astype(int).values
+    part_one(data)
+    part_two(data)
 
 
 if __name__ == '__main__':
